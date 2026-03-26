5cb6c70a8b0e805c1d30e0fc1723b544a99a3f9a
mineztudio <minestudiofficel@gmail.com>
2026-03-26 10:47:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDLFD0a4IWDyC5jpB8Xw1Atti5M3I-Zg-M",
+  authDomain: "shopping-website-3dde5.firebaseapp.com",
+  projectId: "shopping-website-3dde5",
+  storageBucket: "shopping-website-3dde5.firebasestorage.app",
+  messagingSenderId: "286025680401",
+  appId: "1:286025680401:web:d14e0384b9e35ff69993b0",
+  measurementId: "G-Q54SZ2MCMR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
 
 let products = [];
 let cart = [];
